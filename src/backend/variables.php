66e9e2b0fdae19ae26ae2c1b5017a3119ce46446
BenjamimCS <uj1kjfomy@mozmail.com>
2025-04-26 23:26:21 -0300

    fix: append description to HTTP code

--- src/backend/variables.php
+++ src/backend/variables.php
@@ -6,8 +6,8 @@ define('CLOUD_STORAGE_DIR', '../../../../../Downloads/Cloud_Storage/');
 define('HTTP_CODE_TITLE', [
   '200' => '200 Ok',
   '201' => '201 Created',
-  '400' => 'Bad Request',
-  '404' => 'Not Found',
+  '400' => '400 Bad Request',
+  '404' => '404 Not Found',
   '500' => '500 Internal Server Error',
   # TODO: implement the others
 ]);
