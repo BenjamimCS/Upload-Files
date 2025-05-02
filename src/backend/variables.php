5f9be3b221a6fe2520aac38f474dd6685758fad9
BenjamimCS <uj1kjfomy@mozmail.com>
2025-05-02 03:14:45 -0300

    backend: add constant array for HTTP error tips

--- src/backend/variables.php
+++ src/backend/variables.php
@@ -16,3 +16,23 @@ define('HTTP_CODE_TITLE', [
   '501' => '501 Not Implemented',
   # TODO: implement the others
 ]);
+define('HTTP_RESPONSE_TIPS', [
+  '400' => <<<END
+    The server couldn't accomplish the request. Be sure the
+    <code>filename</code> query string is available in the URL.
+    END,
+  '404' => <<<END
+    File not found, do access
+    <a href='/files'
+       class='text-slate-300 underline'><code>/files</code></a>
+    to get a list of the available files.
+    END,
+  '405' => <<<END
+    Only the required HTTP methods are available for use.
+    END,
+  '500' => <<<END
+    Looks like the server is running on trouble. Contact
+     <a href='mailto:uj1kjfomy@mozmail.com' class='text-slate-400 underline'>
+      uj1kjfomy@mozmail.com</a> and let him know.
+    END,
+]);
